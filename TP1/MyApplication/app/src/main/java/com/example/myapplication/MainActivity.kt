fa10abd9b13844dea81805ea2031bd8ad3121bd7
Hashual <loizeau.dorian05@gmail.com>
2025-01-24 11:29:03 +0100

    End of TP1

--- TP1/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ TP1/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.Layout
 import android.widget.Button
@@ -10,6 +11,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
+const val EXTRA_TEXT = "text_to_display"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,15 +27,21 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+
+
         val premierBouton : Button = findViewById(R.id.buttonclic)
+        val next : Button = findViewById(R.id.next)
+
         val premierLayout : EditText = findViewById(R.id.editText)
         val premieraffichage : TextView = findViewById(R.id.affichage)
         val layoutPrincipal : ConstraintLayout = findViewById(R.id.main)
         val deuxiemeTextView : TextView = TextView(this)
+        var validerClick : Boolean = false
 
 
         premierBouton.setOnClickListener {
             premieraffichage.setText(premierLayout.text)
+            validerClick = true
 
             if (premierLayout.text.toString() == "afficher nouveau textView"){
                 deuxiemeTextView.setText(premierLayout.text)
@@ -40,6 +49,18 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+
+        next.setOnClickListener{
+
+            if ( validerClick ){
+                val intent = Intent(this@MainActivity, MainActivity2::class.java)
+                intent.putExtra(EXTRA_TEXT, premieraffichage.text.toString())
+                startActivity(intent)
+
+            }
+        }
+
+
 
     }
 }
