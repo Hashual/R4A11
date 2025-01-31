256405ee68de8f7b2065409741adc32d80a4874d
Hashual <145881009+Hashual@users.noreply.github.com>
2025-01-31 16:35:33 +0100

    End of first activity

---
+++ app/src/main/java/com/example/tp2/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.example.tp2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.tp2.ui.theme.TP2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TP2Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                        ) {
+
+                            Greeting(
+                                modifier = Modifier
+                                    .padding(innerPadding)
+
+                            )
+                            Spacer(modifier = Modifier.height(24.dp))
+                            var text by remember { mutableStateOf("") }
+                            titleText(
+                                text = text,
+                                modifier = Modifier.padding(innerPadding),
+                            )
+                            Spacer(modifier = Modifier.height(5.dp))
+                            TextField(
+                                value = text,
+                                onValueChange = { text=it },
+                                label = { Text("Saisir votre nom") },
+                                modifier = Modifier.padding(innerPadding)
+                            )
+                            Button(
+                                onClick = { },
+                                modifier = Modifier.padding(innerPadding)
+                            ) {
+                                Text("Valider")
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier) {
+    Text(
+        text = "Bienvenue",
+        modifier = modifier,
+        fontWeight = FontWeight(1000)
+    )
+}
+
+@Composable
+fun  titleText(text: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "$text",
+        modifier = modifier,
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TP2Theme {
+    }
+}
