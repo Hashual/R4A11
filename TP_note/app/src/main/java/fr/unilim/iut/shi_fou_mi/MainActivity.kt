9d1fdd31ac3f7bbc01d3956d1e593bcab2b4405c
Florian Claux <159036680+fclaux@users.noreply.github.com>
2025-02-12 09:28:05 +0100

    add v1 navigation

--- TP_note/app/src/main/java/fr/unilim/iut/shi_fou_mi/MainActivity.kt
+++ TP_note/app/src/main/java/fr/unilim/iut/shi_fou_mi/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import fr.unilim.iut.shi_fou_mi.ui.theme.Shi_fou_miTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +30,68 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Shi_fou_miTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNavigation()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AppNavigation(){
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+            HomeScreen(navController = navController)
+        }
+        composable("play") {
+            PlayScreen(navController = navController)
+        }
+    }
+}
+
+@Composable
+fun HomeScreen(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Bienvenue dans le Shake-Fou-Mi",
+            style = MaterialTheme.typography.titleMedium)
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(onClick = { navController.navigate("play") }) {
+            Text(text = "Jouer")
+        }
+
+    }
+}
+
+@Composable
+fun PlayScreen(navController: NavController){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Page de jeu",
+            style = MaterialTheme.typography.titleMedium
+        )
+        Button(onClick = { navController.popBackStack() }) {
+            Text(text = "Retour")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Shi_fou_miTheme {
-        Greeting("Android")
+        AppNavigation()
     }
 }
