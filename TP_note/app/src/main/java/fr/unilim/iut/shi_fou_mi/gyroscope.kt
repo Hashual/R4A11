7063603312fe59ac8ac2816ece50f12f4fa27a3b
Hashual <loizeau.dorian05@gmail.com>
2025-02-12 19:34:40 +0100

    WIP of gyroscope

---
+++ TP_note/app/src/main/java/fr/unilim/iut/shi_fou_mi/gyroscope.kt
@@ -0,0 +1,17 @@
+package fr.unilim.iut.shi_fou_mi
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorManager
+import android.util.Half.EPSILON
+import androidx.core.content.ContextCompat.getSystemService
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.sqrt
+
+class gyroscope {
+
+
+}
+
