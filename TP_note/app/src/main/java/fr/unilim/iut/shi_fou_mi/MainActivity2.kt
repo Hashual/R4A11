5ef5e24ed597e05d0a833a9c4fc30eaf1a604356
Hashual <loizeau.dorian05@gmail.com>
2025-02-18 11:18:01 +0100

    Change name of files to don't have conflict

--- TP_note/app/src/main/java/fr/unilim/iut/shi_fou_mi/MainActivity2.kt
+++ TP_note/app/src/main/java/fr/unilim/iut/shi_fou_mi/MainActivity2.kt
@@ -11,9 +11,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
