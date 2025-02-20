666d86013e8f4e68f12d86b9ed3b3d6a5b7ed893
Florian Claux <florian.claux@etu.unilim.fr>
2025-02-20 13:36:14 +0100

    create customButton, add font, improve home page

--- TP_note/app/src/main/java/fr/unilim/iut/shi_fou_mi/ui/screens/HomeScreen.kt
+++ TP_note/app/src/main/java/fr/unilim/iut/shi_fou_mi/ui/screens/HomeScreen.kt
@@ -1,36 +1,70 @@
 package fr.unilim.iut.shi_fou_mi.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import fr.unilim.iut.shi_fou_mi.R
+import fr.unilim.iut.shi_fou_mi.ui.components.CustomButton
+import fr.unilim.iut.shi_fou_mi.ui.components.MainTitle
+
+val titleFont = FontFamily(
+    Font(R.font.musashi, FontWeight.Normal, FontStyle.Normal),
+)
 
 @Composable
 fun HomeScreen(navController: NavController) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "Bienvenue dans le Shake-Fou-Mi",
-            style = MaterialTheme.typography.titleMedium
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.background_game),
+            contentDescription = "background",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.matchParentSize()
         )
-        Spacer(modifier = Modifier.height(24.dp))
-        Button(onClick = { navController.navigate("play") }) {
-            Text(text = "Jouer")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(80.dp))
+            MainTitle()
+        }
+
+        Box(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .offset(y = (LocalConfiguration.current.screenHeightDp * 0.6).dp)
+        ) {
+            CustomButton(
+                onClick = { navController.navigate("play") },
+                text = "JOUER",
+                padV = 16,
+                padH = 32,
+                textSize = 24
+            )
         }
     }
 }
+
+
+
+
