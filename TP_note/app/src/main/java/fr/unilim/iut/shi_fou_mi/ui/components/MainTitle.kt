d96f228e27fb33c05f26f60ffcd64a64736ecb6a
Florian Claux <159036680+fclaux@users.noreply.github.com>
2025-02-26 16:45:05 +0100

    fix(MainTitle) : center text for small device

--- TP_note/app/src/main/java/fr/unilim/iut/shi_fou_mi/ui/components/MainTitle.kt
+++ TP_note/app/src/main/java/fr/unilim/iut/shi_fou_mi/ui/components/MainTitle.kt
@@ -1,21 +1,29 @@
 package fr.unilim.iut.shi_fou_mi.ui.components
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import fr.unilim.iut.shi_fou_mi.ui.screens.titleFont
 
 @Composable
 fun MainTitle() {
-    Box(modifier = Modifier.padding(16.dp)) {
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .padding(16.dp),
+        contentAlignment = Alignment.Center
+    )
+    {
 
         Text(
             text = "Shake-fou-mi",
@@ -28,6 +36,7 @@ fun MainTitle() {
                     blurRadius = 3f
                 )
             ),
+            textAlign = TextAlign.Center,
             color = Color.Transparent
         )
 
@@ -42,6 +51,7 @@ fun MainTitle() {
                     blurRadius = 3f
                 )
             ),
+            textAlign = TextAlign.Center,
             color = Color(0xFFFFF59D) // Jaune pâle
         )
     }
