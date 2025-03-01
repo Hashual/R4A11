5da5ada3b81545d51c4bf67d470bae6343c1eba7
Florian Claux <florian.claux@etu.unilim.fr>
2025-03-01 15:28:13 +0100

    fix all warnings

--- TP_note/app/src/main/java/fr/unilim/iut/shi_fou_mi/logic/GameLogic.kt
+++ TP_note/app/src/main/java/fr/unilim/iut/shi_fou_mi/logic/GameLogic.kt
@@ -1,12 +1,6 @@
 package fr.unilim.iut.shi_fou_mi.logic
 
-import androidx.compose.runtime.MutableIntState
-import fr.unilim.iut.shi_fou_mi.R
-
-abstract class GameLogic() {
-    protected val rightSymbols = listOf(R.drawable.rock_right, R.drawable.paper_right, R.drawable.cisors_right)
-    protected val leftSymbols = listOf(R.drawable.rock_left, R.drawable.paper_left, R.drawable.cisors_left)
-
+abstract class GameLogic {
     fun launchRound() : Pair<Weapon, Weapon> {
         return applyGameLogic()
     }
--- TP_note/app/src/main/java/fr/unilim/iut/shi_fou_mi/logic/games/ClassicGameLogic.kt
+++ TP_note/app/src/main/java/fr/unilim/iut/shi_fou_mi/logic/games/ClassicGameLogic.kt
@@ -1,11 +1,10 @@
 package fr.unilim.iut.shi_fou_mi.logic.games
 
-import androidx.compose.runtime.MutableIntState
 import fr.unilim.iut.shi_fou_mi.logic.GameLogic
 import fr.unilim.iut.shi_fou_mi.logic.Weapon
 import fr.unilim.iut.shi_fou_mi.logic.weapons.WeaponRepository
 
-class ClassicGameLogic() : GameLogic() {
+class ClassicGameLogic : GameLogic() {
     override fun applyGameLogic(): Pair<Weapon, Weapon> {
         return WeaponRepository.weapons.random() to WeaponRepository.weapons.random()
     }
