0277ed864c1d58bed1ec02624ab8ac6d7f578e06
Florian Claux <florian.claux@etu.unilim.fr>
2025-03-05 14:54:28 +0100

    fix strategies

--- TP_note/app/src/main/java/fr/unilim/iut/shi_fou_mi/logic/strategies/AdaptiveStrategy.kt
+++ TP_note/app/src/main/java/fr/unilim/iut/shi_fou_mi/logic/strategies/AdaptiveStrategy.kt
@@ -2,25 +2,12 @@ package fr.unilim.iut.shi_fou_mi.logic.strategies
 
 import fr.unilim.iut.shi_fou_mi.logic.Strategy
 import fr.unilim.iut.shi_fou_mi.logic.Weapon
-import fr.unilim.iut.shi_fou_mi.logic.weapons.Cisors
-import fr.unilim.iut.shi_fou_mi.logic.weapons.Paper
-import fr.unilim.iut.shi_fou_mi.logic.weapons.Rock
 import fr.unilim.iut.shi_fou_mi.logic.weapons.WeaponRepository
 
 class AdaptiveStrategy : Strategy() {
     override fun getMove(opponentHistory: List<Weapon>): Weapon {
-        if (opponentHistory.isEmpty()) return WeaponRepository.weapons.random()
-
-        if (opponentHistory.size >= 2) {
-            val lastTwoMoves = opponentHistory.takeLast(2)
-            if (lastTwoMoves[0]::class == lastTwoMoves[1]::class) {
-                return when (lastTwoMoves[1]) {
-                    is Rock -> Paper()
-                    is Paper -> Cisors()
-                    is Cisors -> Rock()
-                    else -> WeaponRepository.weapons.random()
-                }
-            }
+        if (opponentHistory.size >= 2 && opponentHistory.takeLast(2).map { it::class }.distinct().size == 1) {
+            return opponentHistory.last().getCounterMove()
         }
 
         return WeaponRepository.weapons.random()
