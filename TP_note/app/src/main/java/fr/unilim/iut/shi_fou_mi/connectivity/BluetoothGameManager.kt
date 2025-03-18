77cf72dcef706ed193fa43de4a85d6487da9197f
Hashual <145881009+Hashual@users.noreply.github.com>
2025-03-18 13:54:14 +0100

    Add request to necessary permissions

--- TP_note/app/src/main/java/fr/unilim/iut/shi_fou_mi/connectivity/BluetoothGameManager.kt
+++ TP_note/app/src/main/java/fr/unilim/iut/shi_fou_mi/connectivity/BluetoothGameManager.kt
@@ -1,16 +1,22 @@
 package fr.unilim.iut.shi_fou_mi.connectivity
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.requestPermissions
+import androidx.core.content.ContextCompat
 
-@SuppressLint("MissingPermission")
+
 class BluetoothGameManager (context: Context, activity : Activity) {
     val REQUEST_ENABLE_BT = 1
-
 
     val bluetoothManager: BluetoothManager = context.getSystemService(BluetoothManager::class.java)
     val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.adapter
@@ -22,10 +28,27 @@ class BluetoothGameManager (context: Context, activity : Activity) {
         if (bluetoothAdapter == null) {
             // Device doesn't support Bluetooth
         }
+        println(bluetoothAdapter?.isEnabled)
         if (bluetoothAdapter?.isEnabled == false) {
             val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             activity.startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
         }
+        else{
+
+            if (ContextCompat.checkSelfPermission(activity, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(activity,
+                        Manifest.permission.BLUETOOTH_CONNECT)) {
+                } else {
+                    ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.BLUETOOTH_CONNECT) , 0);
+                }
+            } else {
+                // Permission has already been granted
+            }
+
+        }
+
+        println("BluetoothGameManager created")
+
     }
 
 
