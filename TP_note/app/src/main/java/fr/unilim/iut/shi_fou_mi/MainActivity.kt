4fd835586fff3ee19f619b896ed25b22376cd003
Hashual <loizeau.dorian05@gmail.com>
2025-03-18 09:48:52 +0100

    Add check to start application only if bluetooth is enabled

--- TP_note/app/src/main/java/fr/unilim/iut/shi_fou_mi/MainActivity.kt
+++ TP_note/app/src/main/java/fr/unilim/iut/shi_fou_mi/MainActivity.kt
@@ -5,13 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
+import fr.unilim.iut.shi_fou_mi.connectivity.BluetoothGameManager
 import fr.unilim.iut.shi_fou_mi.ui.AppNavigation
 import fr.unilim.iut.shi_fou_mi.ui.theme.Shi_fou_miTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val bluetooth = BluetoothGameManager(this.baseContext, this)
+
         enableEdgeToEdge()
         setContent {
             Shi_fou_miTheme {
